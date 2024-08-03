c85b4747f7dd9d6f92a1179e4fcce89bf134664d
Michael Senkao <senkaomichael@gmail.com>
2024-08-03 20:48:51 +0530

    Initial Commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,3 @@ import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
-
-/*
------------------ TODOS -----------------------
-1. ADD 'EDIT' FEATURE
-2. FIX ALL BUGS ON THE FRONTEND
-3. CREATE DATABASE TO STORE NOTES
-4. SETUP BACKEND SERVER TO GET NOTES FROM DATABASE AND SEND TO THE FRONT END
-*/
